028c8dda10fbcb61d7a1491e362b0fe1ec8aedf0
Kaegan Davis Haidome <obichi@coseo.com>
2022-11-30 17:13:36 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3i
+#!/usr/bin/python3
 # Author - Davis Haidome
 
-def islower(c):
-    """Function checks for lowercase characters."""
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True
-    else:
-        return False
+def uppercase(str):
+    """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
