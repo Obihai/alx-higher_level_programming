0655bbd723ff14578694e41259c06c199b3200b9
Kaegan Davis Haidome <obichi@coseo.com>
2023-04-25 19:55:48 +0100

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
 
-const urlDisplay = process.argv[2];
+const url = process.argv[2];
 
-request(urlDisplay, function (err, response) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('code: ${response.statusCode}');
-    }
+request(url, function (error, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
