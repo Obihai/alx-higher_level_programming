2704cba055f17301a15a7330bd749c00eeed7cc3
Obihai <obichi@coseo.com>
2023-04-25 09:59:35 -0700

    Write a string to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Get the file path from the command-line arguments
+const filePath = process.argv[2];
+
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, 'utf-8', function (err) {
+    if (err) {
+        console.error(err);
+    }
+});
