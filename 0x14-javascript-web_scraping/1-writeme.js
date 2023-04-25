4faca5289b25e72d144911c2ffb4cb41d469e70e
Kaegan Davis Haidome <obichi@coseo.com>
2023-04-25 18:02:26 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-// Get the file path from the command-line arguments
 const filePath = process.argv[2];
-
 const stringToWrite = process.argv[3];
 
 fs.writeFile(filePath, stringToWrite, 'utf-8', function (err) {
