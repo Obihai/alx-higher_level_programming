94dbb8ccfba672a7f7f8951dd0f16db269b22dd6
Kaegan Davis Haidome <obichi@coseo.com>
2023-04-25 14:56:41 +0100

    Print out the contents of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 // Read the contents of the file
-fs.readFile(filePath, 'utf-8', function(err, data) {
+fs.readFile(filePath, 'utf-8', function (err, data) {
   if (err) {
     console.error(err);
   } else {
